40a62c80179376740ee7e8b172715fc909b1287f
Akram Helil <akramhelil@gmail.com>
2019-01-22 23:09:03 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -42,7 +42,7 @@ puts "Please enter a song name or number:"
 song_to_play = gets.chomp
 if (1..9).to_a.include?(song_to_play.to_i)
   puts "Playing #{songs[song_to_play.to_i - 1]}"
-elsif song.include?(song_to_play)
+elsif songs.include?(song_to_play)
   puts "Playing #{song_to_play}"
 else
   puts "Invalid input, please try again"
